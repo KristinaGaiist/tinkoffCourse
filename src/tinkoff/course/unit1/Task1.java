f9d944dfdc7b9261d2d70f57a93567c4596bb7ce
k.kirinyuk <kristina.gaiist@yandex.ru>
2021-01-26 17:15:52 +0300

    add main to each task, fix task 1 and task 4, task 5: optimize size and add toString to Note

--- src/tinkoff/course/unit1/Task1.java
+++ src/tinkoff/course/unit1/Task1.java
@@ -2,20 +2,20 @@ package unit1;
 
 public class Task1 {
 
-    public int[] arrayOfEvenNumbersCase1() {
+    private static int[] arrayOfEvenNumbersCase1() {
         int size = 100;
         int evenNumber = 0;
         int[] array = new int[size];
 
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             evenNumber += 2;
             array[i] = evenNumber;
         }
 
-        return array;
+        return multiplyArrayElements(array);
     }
 
-    public int[] arrayOfEvenNumbersCase2() {
+    private static int[] arrayOfEvenNumbersCase2() {
         int size = 100;
         int evenNumber = 0;
         int[] array = new int[size];
@@ -27,29 +27,43 @@ public class Task1 {
             i++;
         }
 
-        return array;
+        return multiplyArrayElements(array);
     }
 
-    public int[] arrayOfEvenNumbersCase3() {
+    private static int[] arrayOfEvenNumbersCase3() {
         int size = 100;
         int evenNumber = 0;
         int[] array = new int[size];
         int i = 0;
 
-        do{
+        do {
             evenNumber += 2;
             array[i] = evenNumber;
             i++;
         }
         while (i < size);
 
-        return array;
+        return multiplyArrayElements(array);
     }
 
-    public void printArray(int[] array) {
-        for(int number : array) {
+    private static void printArray(int[] array) {
+        for (int number : array) {
             System.out.print(number + " ");
         }
         System.out.println();
+    }
+
+    private static int[] multiplyArrayElements(int[] array) {
+        int[] result = new int[array.length - 1];
+        for (int i = 1; i < array.length; i++) {
+            result[i - 1] = array[i] * array[i - 1];
+        }
+        return result;
+    }
+
+    public static void main(String... args) {
+        printArray(arrayOfEvenNumbersCase1());
+        printArray(arrayOfEvenNumbersCase2());
+        printArray(arrayOfEvenNumbersCase3());
     }
 }

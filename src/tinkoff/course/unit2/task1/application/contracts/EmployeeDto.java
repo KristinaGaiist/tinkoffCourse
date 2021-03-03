49c8978f3d026c8647fa0c42142b1958fe421c88
KristinaGaiist <1997Kris!>
2021-03-03 10:38:02 +0300

    unit 5 reset random changes

--- src/tinkoff/course/unit2/task1/application/contracts/EmployeeDto.java
+++ src/tinkoff/course/unit2/task1/application/contracts/EmployeeDto.java
@@ -1,7 +1,5 @@
 package unit2.task1.application.contracts;
 
-import java.util.Objects;
-
 public class EmployeeDto {
 
     private final String firstName;
@@ -18,23 +16,5 @@ public class EmployeeDto {
 
     public String getFirstName() {
         return firstName;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        EmployeeDto that = (EmployeeDto) o;
-        return Objects.equals(firstName, that.firstName) &&
-            Objects.equals(lastName, that.lastName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lastName);
     }
 }
